5f5b736308bfdfb15af84fe7986d252b7ef0990b
Christian Muirhead <christian.muirhead@canonical.com>
2016-07-27 10:50:37 +0100

    Tweaked to create a vm for maas 2 installation

--- setup-vm.sh
+++ setup-vm.sh
@@ -5,7 +5,7 @@ set -e
 for i in "$@"; do
     name=vm-${i}
     virsh pool-refresh default
-    virsh vol-clone --pool default trusty-server-cloudimg-amd64-disk1.img ${name}.img
+    virsh vol-clone --pool default xenial-server-cloudimg-amd64-disk1.img ${name}.img
     virsh vol-resize --pool default ${name}.img +10G
     virsh pool-refresh default
     virt-install -r 1024 \
@@ -16,6 +16,6 @@ for i in "$@"; do
     		 --boot hd \
     		 --disk vol=default/${name}.img,format=qcow2,bus=virtio,cache=writeback,size=${DISK_SIZE:-10} \
     		 --disk vol=default/${name}-config.iso,bus=virtio \
-                 --network network=maas,model=virtio
+                 --network network=maas2,model=virtio
 
 done
